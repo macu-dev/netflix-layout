a83a3a865006836cde21a7f9adb5195602f8666a
Maria Claudia <mariacladiaperezes@gmail.com>
2020-10-19 08:57:40 -0300

    se agrego la funcionalidad del carrusel

--- js/app.js
+++ js/app.js
@@ -1,25 +1,46 @@
-
-const btnSlideRight = document.querySelector("#right");
-const btnSlideLeft = document.querySelector("#left");
+const menuItems = document.querySelectorAll(".item");
 const row = document.querySelector(".gallery");
 const movies = document.querySelectorAll(".gallery-item");
+const arrowGalleryLeft = document.querySelectorAll(".slider-btn-left");
+const arrowGalleryRigth = document.querySelectorAll(".slider-btn-right");
 
-// btnSlideLeft.addEventListener("click", slider_left);
-// btnSlideRight.addEventListener("click", slider_right);
+function addControllersToMenuButtons(butons, controller) {
+  butons.forEach(b => b.addEventListener('click', controller));
+}
+
+function controllerMenuButton(e) {
+  let item = e.target;
+
+  if(item.nodeName == 'I'){
+    item = item.parentNode;
+  }
+
+  item.classList.add('item-active');
+  menuItems.forEach(otherItem => {
+   if (otherItem != item)
+     otherItem.classList.remove('item-active');
+  });
+}
+
+function btnSliderLeft(e){
+  let element = e.target.nextElementSibling;
+  element.scrollLeft += element.offsetWidth;
+}
+
+function btnSliderRight(e){
+  let element = e.target.previousElementSibling;
+   element.scrollLeft -= element.offsetWidth;
+}
 
 
+movies.forEach(movie => {
+  movie.addEventListener('mouseenter', () => {
+    movie.classList.add('hover');
+    movie.addEventListener('mouseleave',() => movie.classList.remove('hover'));
+  });
+});
 
-// function slider_left(){
-//   row.scrollLeft += row.offsetWidth;
-// }
 
-// function slider_right(){
-//   row.scrollLeft -= row.offsetWidth;
-// }
-
-// movies.forEach(movie => {
-//   movie.addEventListener('mouseenter', () => {
-//     movie.classList.add('hover');
-//     movie.addEventListener('mouseleave',() => movie.classList.remove('hover'));
-//   });
-// });
+addControllersToMenuButtons(menuItems, controllerMenuButton);
+addControllersToMenuButtons(arrowGalleryLeft, btnSliderLeft);
+addControllersToMenuButtons(arrowGalleryRigth, btnSliderRight);

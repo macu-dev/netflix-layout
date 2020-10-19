f55ea8b6829d95d48873aef1232eb35c3900b1e8
Maria Claudia <mariacladiaperezes@gmail.com>
2020-10-18 22:12:23 -0300

    se termino con las mediasqueries

--- js/app.js
+++ js/app.js
@@ -4,22 +4,22 @@ const btnSlideLeft = document.querySelector("#left");
 const row = document.querySelector(".gallery");
 const movies = document.querySelectorAll(".gallery-item");
 
-btnSlideLeft.addEventListener("click", slider_left);
-btnSlideRight.addEventListener("click", slider_right);
+// btnSlideLeft.addEventListener("click", slider_left);
+// btnSlideRight.addEventListener("click", slider_right);
 
 
 
-function slider_left(){
-  row.scrollLeft += row.offsetWidth;
-}
+// function slider_left(){
+//   row.scrollLeft += row.offsetWidth;
+// }
 
-function slider_right(){
-  row.scrollLeft -= row.offsetWidth;
-}
+// function slider_right(){
+//   row.scrollLeft -= row.offsetWidth;
+// }
 
-movies.forEach(movie => {
-  movie.addEventListener('mouseenter', () => {
-    movie.classList.add('hover');
-    movie.addEventListener('mouseleave',() => movie.classList.remove('hover'));
-  });
-});
+// movies.forEach(movie => {
+//   movie.addEventListener('mouseenter', () => {
+//     movie.classList.add('hover');
+//     movie.addEventListener('mouseleave',() => movie.classList.remove('hover'));
+//   });
+// });
